1fac660312a019fe1be51e8ea89c845c790189b2
Laura <flaura42@gmail.com>
2020-07-20 20:59:36 -0700

    adjust packages

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,19 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const regeneratorRuntime = require("regenerator-runtime");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
   mode: 'development',
   devtool: 'source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   stats: 'verbose',
   module: {
     rules: [
@@ -23,6 +30,7 @@ module.exports = {
   output: {
     libraryTarget: 'var',
     library: 'Client',
+    path: path.resolve(process.cwd(), 'dist')
   },
   plugins: [
     new HtmlWebPackPlugin({
@@ -37,6 +45,7 @@ module.exports = {
       // Automatically remove all unused webpack assets on rebuild
       cleanStaleWebpackAssets: true,
       protectWebpackAssets: false
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 }
